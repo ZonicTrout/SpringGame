e7b7cb8c9d925e0564244ebd634d4e6ffca33343
ZonicTrout <chesskingmj@gmail.com>
2023-03-27 21:22:07 -0700

    Testing in main function

--- spring.cpp
+++ spring.cpp
@@ -4,5 +4,15 @@
 
 int main( void ) {
     Game first( "first" );
+    std::cout << first.mainPlayer->playerInventory->amount << std::endl;
+    ItemsStats Sword{
+        .name="Sword",
+        .description="A light steel sword that is better than a stick",
+        .attack=5,
+        .defence=0,
+        .cost=5
+    };
+    first.mainPlayer->playerInventory->addItem(&Sword);
+    std::cout << first.mainPlayer->playerInventory->amount << std::endl;
 }
 
